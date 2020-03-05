20145f12c13f206558695695d5e3ed2a4d1ba418
ks64455 <ks64455@gmail.com>
2020-03-05 13:29:18 +0900

    상품 등록 기능 추가

---
+++ WebContent/mngr/productProcess/bookregist.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	//[책 등록] 버튼 클릭
+	$("upForm1").ajaxForm({//이미지를 포함한 상품 등록
+		success: function(data, status) { //업로드에 성공하면 수행
+			window.location.href("/shoppingmall/mg/bookList.do?book_kind=all");
+		}
+	}); //[책 등록] 버튼 클릭 end
+	
+	
+	//[관리자 메인으로]버튼 클릭
+	$("bookmain").click(function() {
+		window.location.href("/shoppingmall/mg/managerMain.do");
+	});//[관리자 메인으로]버튼 클릭 end
+	
+	//[목록으로] 버튼 클릭
+	$("upForm1").click(function() {
+		window.location.href("/shoppingmall/mg/bookList.do?book_kind=all");
+	}); //[목록으로] 버튼 클릭 end
+});
